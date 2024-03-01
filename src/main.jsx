53901b80a1da44bf3023b479bb4e4bf6be6f86a7
Estevan <egonz94@gmail.com>
2024-03-01 15:37:55 -0600

    Added Monopoly

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,24 @@ import Root from "./routes/root";
 import './App.css'
 import ErrorPage from '../error-page';
 import Projects from './pages/Projects';
+import Monopoly from './pages/Monopoly';
 
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Root />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: '/Projects',
-      element: <Projects />,
-    },
-  ],
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/Projects',
+    element: <Projects />,
+  },
+  {
+    path: 'Projects/monopoly',
+    element: <Monopoly />
+  }
+],
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ src/pages/Monopoly.jsx
@@ -0,0 +1,313 @@
+
+import '../assets/monopoly.css'
+
+function Monopoly() {
+
+  return (
+    <div className="outer-monopoly">
+
+  <div className="grid-monopoly">
+
+  <div className="property-free-parking">
+    <div className="rotate-top-left">
+      Free Parking
+    </div>
+  </div>
+  <div className="property-body">
+    <div className="property-text-top">
+      KENTUCKY AVENUE
+    </div>
+  </div>
+  <div className="property-chance-top">
+    <div className="rotate-top">
+      Chance
+    </div>
+  </div>
+  <div className="property-body">
+    <div className="property-text-top">
+      INDIANA AVENUE
+    </div>
+  </div>
+  <div className="property-body">
+    <div className="property-text-top">
+      ILLINOIS AVENUE
+    </div>
+  </div>
+  <div className="property-bo-railroad">
+  <div className="rotate-top">
+      B.&O. Railroad
+    </div>
+  </div>
+  <div className="property-body">
+    <div className="property-text-top">
+      ATLANTIC AVENUE
+    </div>
+  </div>
+  <div className="property-body">
+    <div className="property-text-top">
+      VENTOR AVENUE
+    </div>
+  </div>
+  <div className="property-water-works">
+    <div className="rotate-top">
+      Water Works
+    </div>
+  </div>
+  <div className="property-body">
+    <div className="property-text-top">
+      MARVIN AVENUE
+    </div>
+  </div>
+  <div className="property-go-to-jail">
+    <div className="rotate-top-right">
+      Go to Jail
+    </div>
+  </div>
+  <div className="property-header property-red"></div>
+  <div className="property-header property-red"></div>
+  <div className="property-header property-red"></div>
+  <div className="property-header property-yellow"></div>
+  <div className="property-header property-yellow"></div>
+  <div className="property-header property-yellow"></div>
+
+  <div className="property-body">
+    <div className="rotate-left">
+      NEW YORK AVENUE
+    </div>
+  </div>
+  <div className="property-header property-orange"></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-header property-green"></div>
+  <div className="property-body">
+    <div className="rotate-right">
+        Pacific Avenue
+    </div>
+  </div>
+
+
+
+
+  <div className="property-body">
+    <div className="rotate-left">
+      Tenneesee Avenue
+    </div>
+  </div>
+  <div className="property-header property-orange"></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-header property-green"></div>
+  <div className="property-body">
+    <div className="rotate-right">
+        North Carolina Avenue
+    </div>
+  </div>
+
+  <div className="property-cc-left">
+    <div className="rotate-left-full">
+        Community Chest
+      </div>
+  </div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-cc-right">
+    <div className="rotate-right-full">
+      Community Chest
+    </div>
+  </div>
+  <div className="property-body">
+    <div className="rotate-left">
+      St. James Place
+    </div>
+  </div>
+  <div className="property-header property-orange"></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-header property-green"></div>
+  <div className="property-body">
+    <div className="rotate-right">
+        Pennsyl. Avenue
+    </div>
+  </div>
+
+  <div className="property-pennsylvania-railroad">
+    <div className='rotate-left-full'>
+    Pennsyl. Railroad
+    </div>
+  </div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-short-line">
+    <div className="rotate-right-full">
+      Short Line
+    </div>
+  </div>
+
+  <div className="property-body">
+    <div className="rotate-left">
+      Viriginia Avenue
+    </div>
+  </div>
+  <div className="property-header property-hotpink"></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-chance-right">
+  <div className="rotate-right-full">
+      Chance
+    </div>
+  </div>
+
+  <div className="property-body">
+    <div className="rotate-left">
+      States Avenue
+    </div>
+  </div>
+  <div className="property-header property-hotpink"></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-header property-blue"></div>
+  <div className="property-body">
+    <div className="rotate-right">
+        Park Place
+    </div>
+  </div>
+
+  <div className="property-electric-company">
+    <div className="rotate-left-full">
+      Electric Company
+    </div>
+  </div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-luxury-tax">
+  <div className="rotate-right-full">
+      Luxury Tax
+    </div>
+  </div>
+
+  <div className="property-body">
+    <div className="rotate-left">
+      St. Charles Place
+    </div>
+  </div>
+  <div className="property-header property-hotpink"></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div className="property-header property-blue"></div>
+  <div className="property-body">
+    <div className="rotate-right">
+        Boardwalk
+    </div>
+  </div>
+  
+  <div className="property-just-visiting">
+    <div className='rotate-bottom-left'>
+      Just Visiting
+    </div>
+  </div>
+  <div className="property-header property-lightblue"></div>
+  <div className="property-header property-lightblue"></div>
+  <div className="property-chance-bottom">
+      <div className="rotate-bottom">
+        Chance
+      </div>
+    </div>
+  <div className="property-header property-lightblue"></div>
+  <div className="property-reading-railroad">
+    <div className="rotate-bottom">
+        Reading Railroad
+      </div>
+  </div>
+  <div className="property-income-tax">
+    <div className="rotate-bottom">
+        Income Tax
+      </div>
+  </div>
+  <div className="property-header property-purple"></div>
+  <div className="property-cc-bottom">
+    <div className="rotate-bottom">
+        Community Chest
+      </div>
+  </div>
+  <div className="property-header property-purple"></div>
+  <div className="property-go">
+    <div className="rotate-bottom-right">
+      GO
+    </div>
+  </div>
+  <div className="property-body">Connecticut Avenue</div>
+  <div className="property-body">Vermont Avenue</div>
+  <div className="property-body">Oriental Avenue</div>
+  <div className="property-body">Baltic Avenue</div>
+  <div className="property-body">Medit. Avenue</div>
+
+  </div>
+  </div>
+
+  )
+}
+
+export default Monopoly
