777b2d645c94697f552017f19b6a71f5f98f7049
Estevan <egonz94@gmail.com>
2024-03-01 15:00:05 -0600

    Added home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ErrorPage from '../error-page';
+
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+    }
+  ],
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/root.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+import '/Users/estevan/Desktop/estevan/src/assets/index.css'
+
+export default function Root() {
+  return (
+    <div className="outside-main">
+
+        <div className="homepage-header">Estevan Gonzales</div>
+        <div className="homepage-subline">Data Analyst & Software Developer</div>
+
+
+      <div className="homepage-double">
+        <div></div>
+        <div className="homepage-image">
+        </div>
+
+        <div className="homepage-links">
+          <div className="homepage-link">
+            <Link to="/Projects">Projects</Link>
+          </div>
+          <div className="homepage-link">
+            <Link to="/DataSets">Data Sets</Link>
+          </div>
+          <div className="homepage-link">
+            <Link to="/Contact">Contact</Link>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  );
+}
