3b02fb2a28c48b14cafcfc968653586caa240076
Estevan <egonz94@gmail.com>
2024-03-08 19:10:06 -0600

    Added oscars

--- src/pages/Visualizations.jsx
+++ src/pages/Visualizations.jsx
@@ -8,6 +8,12 @@ export default function Visualizations() {
             id: "presidents",
             url: "https://public.tableau.com/app/profile/estevan.gonzales8848/viz/USPresidents_17097719423380/Sheet1",
             image_url: "/images/presidents.png"
+        },
+        {
+            name: "Historic Oscar Wins",
+            id: "oscars",
+            url: "https://public.tableau.com/app/profile/estevan.gonzales8848/viz/OscarWins/HistroicWins?publish=yes",
+            image_url: "/images/oscars.png"
         }
     ];
             
