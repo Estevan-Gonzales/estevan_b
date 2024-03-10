be4c27a799c84b5747d38177359e979031026de4
Estevan <egonz94@gmail.com>
2024-03-10 03:45:35 -0500

    Added RD for splash page

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,28 +4,28 @@ import { Link } from 'react-router-dom';
 export default function Root() {
   return (
     <div className="outside-main">
-
-        <div className="homepage-header">Estevan Gonzales</div>
-        <div className="homepage-subline">Data Analyst & Software Developer</div>
-
-
-      <div className="homepage-double">
-        <div></div>
-        <div className="homepage-image">
+        <div className="container">
+          <div className="row homepage-header">Estevan Gonzales</div>
+          <div className="row homepage-subline">Data Analyst & Software Developer</div>
         </div>
 
-        <div className="homepage-links">
-          <div className="homepage-link">
-            <Link to="/Projects">Projects</Link>
-          </div>
-          <div className="homepage-link">
-            <Link to="/Visualizations">Visualizations</Link>
-          </div>
-          <div className="homepage-link">
-            <Link to="/DataSets">Data Sets</Link>
-          </div>
-          <div className="homepage-link">
-            <Link to="/Contact">Contact</Link>
+
+      <div className="container">
+        <div className="row full-width">
+          <div className="col-12 col-md-6"></div>
+          <div className="col-12 col-md-6 homepage-links">
+            <div className="homepage-link">
+              <Link to="/Projects">Projects</Link>
+            </div>
+            <div className="homepage-link">
+              <Link to="/Visualizations">Visualizations</Link>
+            </div>
+            <div className="homepage-link">
+              <Link to="/DataSets">Data Sets</Link>
+            </div>
+            <div className="homepage-link">
+              <Link to="/Contact">Contact</Link>
+            </div>
           </div>
         </div>
       </div>
