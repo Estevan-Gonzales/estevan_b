a40060a23a5344ddab33e097ba2d2845b4da55ee
Estevan <egonz94@gmail.com>
2024-03-15 17:52:16 -0500

    Added resume to Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,13 @@ export default function Contact() {
 
             <div className="outside-main">
                 <div className="pages-header">Contact Me</div>
-
+                    <div class="container">
+                        <div class="row">
+                            <div class="col resume-div">
+                                <img class="resume" src="./images/Gonzales_Resume.png"></img>
+                            </div>
+                        </div>
+                    </div>
             </div>
 
     );
