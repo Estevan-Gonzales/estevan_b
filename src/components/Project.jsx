4b7bf132331e073f5d8aeb71af1115f9cddaffbb
Estevan <egonz94@gmail.com>
2024-10-17 15:32:24 -0500

    Added project details

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,22 +5,28 @@ import '../assets/css/projects.css'
 function Project({ projects }) {
 
     return (
-        <div className="container projects-grid">
-            <div className="row project-single">
+        <div>
+            <div className="container projects-grid">
+                <div className="row project-single">
 
-            {projects.map((project) => (
-                <div className="project col-12 col-md-6 mt-4" key={project.id}>
-                    <Link to={project.id}>
-                        <img className="darken" src={project.image_url}></img>
-                        <div className="project-caption">{project.name}</div>
-                    </Link>
+                {projects.map((project) => (
+                    <div className="project col-12 col-md-6 mt-4" key={project.id}>
+                        <Link to={project.id}>
+                            <img className="darken" src={project.image_url}></img>
+                            <div className="project-name">{project.name}</div>
+                        </Link>
+                        <div className="project-description">{project.description}</div>
+                        <div className="project-technologies-grid">
+                        {project.technologies.map((x) => (
+                            <div className="project-technologies">{x}</div>
+                        ))}
+                        </div>
+                    </div>
+                ))}
                 </div>
-
-        ))}
-
             </div>
-
         </div>
+
     )
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,31 +6,43 @@ import Navbar from '../components/Navbar';
 export default function Projects() {
 
     let projects = [
-        {
-            name: "Cube",
-            id: "cube",
-            image_url: "/images/cube.png"
-        },
-        {
-            name: "Monopoly",
-            id: "monopoly",
-            image_url: "/images/monopoly.png"
-        },
+
         {
             name: "The Elements",
             id: "elements",
             image_url: "/images/elements.png",
+            description: "Interactive periodic table of elements.",
+            technologies: ["CSS/HTML", "Grid", "Bootstrap", "React"]
         },
+        {
+            name: "Cube",
+            id: "cube",
+            image_url: "/images/cube.png",
+            description: "Interactive 2D representation of a standard Rubik's cube.",
+            technologies: ["CSS/HTML", "Grid", "Bootstrap"]
+        },
+        {
+            name: "Monopoly",
+            id: "monopoly",
+            image_url: "/images/monopoly.png",
+            description: "Static recreation of a Monopoly board.",
+            technologies: ["CSS/HTML", "Grid", "Bootstrap"]
+        },
+
         {
             name: "Halfs",
             id: "halfs",
             image_url: "/images/halfs.png",
+            description: "Static recreation of a Monopoly board.",
+            technologies: ["CSS/HTML", "Grid"]
 
         },
         {
             name: "TypeHype",
             id: "typehype",
             image_url: "/images/typehype.png",
+            description: "Digital representation of a keyboard.",
+            technologies: ["CSS/HTML", "Grid",]
         }
     ];
             
