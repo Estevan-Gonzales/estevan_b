39efe1b09c5a0afdaff50313d53a2c872a35a17b
Estevan <egonz94@gmail.com>
2024-10-23 17:42:42 -0500

    Added Add Dice button

--- src/pages/projects/Dice.jsx
+++ src/pages/projects/Dice.jsx
@@ -14,89 +14,111 @@ function Dice() {
     }
 
     function returnDice() {
-        let target = document.getElementsByClassName("dice")[0]
-        target.innerHTML = ""
-        console.log(target)
-        let randomValue = Math.floor(Math.random() * 6) + 1
+        let targets = document.getElementsByClassName("dice")
+        console.log(targets)
+        for (let i=0; i<targets.length; i++) {
 
-        switch (randomValue) {
-            case 1:
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendDot(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                break;
-            case 2:
-                appendDot(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendDot(target)
-                break;
-            case 3:
-                appendDot(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendDot(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendDot(target)
-                break;
-            case 4:
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                appendClear(target)
-                appendClear(target)
-                appendClear(target)
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                break
-            case 5:
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                break
-            case 6:
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                appendDot(target)
-                appendClear(target)
-                appendDot(target)
-                break
+            let target = targets[i]
+            console.log(target)
+            target.innerHTML = ""
+            console.log(target)
+            let randomValue = Math.floor(Math.random() * 6) + 1
+    
+            switch (randomValue) {
+                case 1:
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    break;
+                case 2:
+                    appendDot(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendDot(target)
+                    break;
+                case 3:
+                    appendDot(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendDot(target)
+                    break;
+                case 4:
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    break
+                case 5:
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    break
+                case 6:
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    appendDot(target)
+                    appendClear(target)
+                    appendDot(target)
+                    break
+            }
+
+
         }
-
 
     }
 
+    function addDice() {
+
+        var target = document.getElementById("dice-collection")
+        console.log(target)
+
+        var newDice = document.createElement("div")
+        newDice.classList.add("dice")
+        target.appendChild(newDice)
+        
+    }    
+
     return (
         <div id="dice-master">
-            <div className="dice">
+            <div id="dice-collection">
+                <div className="dice"></div>
+
             </div>
 
             <button type="button" className="btn btn-secondary" onClick={returnDice}>Roll Dice</button>
+
+            <button type="button" className="btn btn-secondary" onClick={addDice}>Add Dice</button>
 
         </div>
 
