034ff13e37eac1869c35255b17385c820268473f
Estevan <egonz94@gmail.com>
2024-12-05 16:58:52 -0600

    Added identifying minute block target

--- src/pages/projects/Clock.jsx
+++ src/pages/projects/Clock.jsx
@@ -11,21 +11,33 @@ function Clock() {
 
             let hourBox = document.createElement('div')
             hourBox.className = "hour-block";
+
             for (let i = 0; i < 60; i++) {
                 let minuteBox = document.createElement('div');
                 minuteBox.className = "minute-block";
+                minuteBox.id = h.toString() + "-" + i.toString()
                 hourBox.append(minuteBox)
+            
             }
             
             grid.append(hourBox);
-
         }
-
-        
-
     }
 
+    function setCurrentMinute() {
+        let now = new Date();
+        let hour = now.getHours();
+        let minute = now.getMinutes();
+        let dateAttribute = hour.toString() + "-" + minute.toString()        
+        let targetMinute = document.getElementById(dateAttribute);
+
+        targetMinute.style.backgroundColor = "black"
+        console.log(targetMinute);
+    }
+
+
     renderMinutes();
+    setCurrentMinute();
 
     }, []);
 
